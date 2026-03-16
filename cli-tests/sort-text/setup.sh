7f8bbefb05d85dcbcc560a0fb23256b912676486
Grail Finder <wohilas@gmail.com>
2026-03-16 06:36:24 +0300

    Chore: add cli test

---
+++ cli-tests/sort-text/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /tmp/sort-text
+
+printf "cat" > /tmp/sort-text/file1.txt
+printf "red" > /tmp/sort-text/file2.txt
+printf "dog" > /tmp/sort-text/file3.txt
+printf "blue" > /tmp/sort-text/file4.txt
